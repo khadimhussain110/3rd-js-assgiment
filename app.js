5da5ae779869b320f65c63b19260c7a3e35d6690
Gohar Abbas <103553225+khadimhussain110@users.noreply.github.com>
2023-02-04 05:28:20 -0800

    Add files via upload
    3rd js assgiment

---
+++ app.js
@@ -0,0 +1,19 @@
+// Chapter#3 JavaScript Assgiment # 3
+
+// question 1
+
+// var age =("I am 20 years old ");
+// alert(age);
+
+// question 2
+
+// var vist =("You have visited this site 14 times");
+// alert(vist);
+
+// question 3
+
+// var birthyear =("my birth year 1992");
+// document.write(birthyear);
+
+// question 4 is panding ?????
+
